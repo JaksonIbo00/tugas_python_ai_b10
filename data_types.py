df9f0b97346d5d6317397d147c4dfd1c116f448b
JaksonIbo <iboromario@gmail.com>
2026-03-30 20:28:47 +0200

    Upload tugas3.py

---
+++ data_types.py
@@ -0,0 +1,22 @@
+# Tipe data :
+# string
+print("hello world")
+
+#int
+print(1000)
+print(-1000)
+
+#float
+print(3.14)
+print(-3.14)
+
+#bool
+print(True)
+print(False)
+
+print(int(0.1)) #hasilnya dibulatkan sesuai ke 
+print(float(True))
+print(float(1))
+print(bool(True))
+print(bool(3685764546))
+print(bool(0))
---
+++ expression_n_variable.py
@@ -0,0 +1,14 @@
+a = 5
+b = 4
+c = "jakson"
+d = "ibo"
+print(a)
+print(b)
+print(c)
+print(a+b)
+print(c+d)
+print(c +" " +d)
+print(c * 5)
+
+umur = 24
+print("umur saya adalah" + " " + str(umur)+ " " + "tahun")
